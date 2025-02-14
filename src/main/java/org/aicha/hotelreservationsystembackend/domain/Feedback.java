4f2e0f11b7781e43ba22f6616b4ad6825a30622b
ettabet aicha <aicha70tabite@gmail.com>
2025-02-14 11:52:01 +0100

    edit feedback

--- src/main/java/org/aicha/hotelreservationsystembackend/domain/Feedback.java
+++ src/main/java/org/aicha/hotelreservationsystembackend/domain/Feedback.java
@@ -2,11 +2,10 @@ package org.aicha.hotelreservationsystembackend.domain;
 
 import jakarta.persistence.*;
 import lombok.*;
-import org.hibernate.proxy.HibernateProxy;
-
 import java.time.LocalDateTime;
-import java.util.Objects;
+
 import java.util.UUID;
+
 
 @Entity
 @Getter
@@ -14,7 +13,9 @@ import java.util.UUID;
 @ToString
 @RequiredArgsConstructor
 public class Feedback {
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(updatable = false, nullable = false)
     private UUID id;
     private Integer rating;
     private String comment;
@@ -26,19 +27,6 @@ public class Feedback {
 
     public void submitFeedback() {}
 
-    @Override
-    public final boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null) return false;
-        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
-        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
-        if (thisEffectiveClass != oEffectiveClass) return false;
-        Feedback feedback = (Feedback) o;
-        return getId() != null && Objects.equals(getId(), feedback.getId());
-    }
 
-    @Override
-    public final int hashCode() {
-        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
-    }
+
 }
