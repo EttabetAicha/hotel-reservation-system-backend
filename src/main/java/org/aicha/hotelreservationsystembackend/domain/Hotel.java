16445cc93830841a79fd12b98908587de94e1abb
ettabet aicha <aicha70tabite@gmail.com>
2025-02-14 11:54:56 +0100

    fix hotel domaine

--- src/main/java/org/aicha/hotelreservationsystembackend/domain/Hotel.java
+++ src/main/java/org/aicha/hotelreservationsystembackend/domain/Hotel.java
@@ -2,10 +2,7 @@ package org.aicha.hotelreservationsystembackend.domain;
 
 import jakarta.persistence.*;
 import lombok.*;
-import org.hibernate.proxy.HibernateProxy;
-
 import java.util.List;
-import java.util.Objects;
 import java.util.UUID;
 
 @Entity
@@ -14,11 +11,14 @@ import java.util.UUID;
 @ToString
 @RequiredArgsConstructor
 public class Hotel {
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(updatable = false, nullable = false)
     private UUID id;
     private String name;
     private String description;
     private String address;
+    private String location;
     private String city;
     private String country;
     private Integer rating;
@@ -31,19 +31,5 @@ public class Hotel {
     private List<Room> rooms;
     public void addRoom(Room room) {}
     public void updateDetails() {}
-    @Override
-    public final boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null) return false;
-        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
-        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
-        if (thisEffectiveClass != oEffectiveClass) return false;
-        Hotel hotel = (Hotel) o;
-        return getId() != null && Objects.equals(getId(), hotel.getId());
-    }
 
-    @Override
-    public final int hashCode() {
-        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
-    }
 }
