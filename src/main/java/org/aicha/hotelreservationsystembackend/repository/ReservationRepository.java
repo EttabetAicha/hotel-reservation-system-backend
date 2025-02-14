5d102c755a375faaffb09d2eb9277a39cc4fcc34
ettabet aicha <aicha70tabite@gmail.com>
2025-02-14 11:54:09 +0100

    adding reservation repo

--- src/main/java/org/aicha/hotelreservationsystembackend/repository/ReservationRepository.java
+++ src/main/java/org/aicha/hotelreservationsystembackend/repository/ReservationRepository.java
@@ -1,19 +1,19 @@
 package org.aicha.hotelreservationsystembackend.repository;
 
 import org.aicha.hotelreservationsystembackend.domain.Reservation;
+import org.aicha.hotelreservationsystembackend.domain.enums.ReservationStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
-import java.util.Date;
 import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface ReservationRepository extends JpaRepository<Reservation, UUID> {
-    List<Reservation> findByUserId(UUID userId);
-    List<Reservation> findByHotelId(UUID hotelId);
-    List<Reservation> findByStatus(String status);
-    List<Reservation> findByCheckInDateBetween(LocalDateTime startDate, Date endDate);
+    List<Reservation> findByClientId(UUID clientId);
+    List<Reservation> findByRoomHotelId(UUID hotelId);
+    List<Reservation> findByStatus(ReservationStatus status);
+    List<Reservation> findByCheckInBetween(LocalDateTime startDate, LocalDateTime endDate);
     List<Reservation> findByRoomId(UUID roomId);
 }
