33ac9a9e859501e00c711d7c4b2c74fc3e81fa79
ettabet aicha <aicha70tabite@gmail.com>
2025-02-24 15:18:31 +0000

    addding attribut for stripe

--- src/main/java/org/aicha/hotelreservationsystembackend/domain/Payment.java
+++ src/main/java/org/aicha/hotelreservationsystembackend/domain/Payment.java
@@ -2,6 +2,7 @@ package org.aicha.hotelreservationsystembackend.domain;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.aicha.hotelreservationsystembackend.domain.enums.PaymentStatus;
 
 import java.util.UUID;
 
@@ -17,12 +18,21 @@ public class Payment {
     private UUID id;
     @ManyToOne
     private Reservation reservation;
-    private Double amount;
+    @Column(nullable = false)
+    private double amount;
+    @Column(nullable = false)
     private String paymentMethod;
-    private String paymentStatus;
+    @Column(name = "stripe_payment_intent_id")
+    private String stripePaymentIntentId;
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private PaymentStatus paymentStatus = PaymentStatus.PENDING;
 
 
-    public boolean processPayment() { return false; }
+    public boolean processPayment() {
+
+        return false;
+    }
 
 
 }
