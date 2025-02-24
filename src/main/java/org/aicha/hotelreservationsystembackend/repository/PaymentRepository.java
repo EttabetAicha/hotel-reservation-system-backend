b1401f42bdda92033f5516b6bee09389021ade26
ettabet aicha <aicha70tabite@gmail.com>
2025-02-24 15:19:38 +0000

    add payment repo

--- src/main/java/org/aicha/hotelreservationsystembackend/repository/PaymentRepository.java
+++ src/main/java/org/aicha/hotelreservationsystembackend/repository/PaymentRepository.java
@@ -1,14 +1,21 @@
 package org.aicha.hotelreservationsystembackend.repository;
 
 import org.aicha.hotelreservationsystembackend.domain.Payment;
+import org.aicha.hotelreservationsystembackend.domain.enums.PaymentStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Collection;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface PaymentRepository extends JpaRepository<Payment, UUID> {
     List<Payment> findByReservationId(UUID reservationId);
     List<Payment> findByPaymentMethod(String paymentMethod);
+
+    List<Payment> findByPaymentStatus(PaymentStatus paymentStatus);
+
+    Optional<Payment> findByStripePaymentIntentId(String paymentIntentId);
 }
